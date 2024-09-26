58b344029c6b0fc92465666855a23b255c73e2dd
xJunko <fireredz69420@gmail.com>
2024-09-26 19:48:24 +0800

    Prototype: Subtexture Reading

--- src/main_farc.v
+++ src/main_farc.v
@@ -91,15 +91,60 @@ fn main() {
 		}
 
 		current := br.position
-		br.position = offset
+		br.position += offset - 16 // Hack
 
-		texture_signature := br.read_u32(true)
+		texture_signature := br.read_u32(false)
 
 		if texture_signature != 0x04505854 && texture_signature != 0x05505854 {
 			panic('Invalid Texture Signature: ${texture_signature}')
 		}
 
-		println(texture_signature) // Signature
+		sub_text_count := br.read_u32(false)
+		println(sub_text_count) // subTextureCount
+
+		sub_text_info := br.read_u32(false)
+		println(sub_text_info) // subTextInfo
+
+		mut mip_map_count := sub_text_info & 0xFF
+		array_size := (sub_text_info >> 8) & 0xFF
+
+		if array_size == 1 && mip_map_count != sub_text_count {
+			mip_map_count = u8(sub_text_count)
+		}
+
+		for j := 0; j < array_size; j++ {
+			for k := 0; k < mip_map_count; k++ {
+				mipmap_offset := br.read_u32(false)
+
+				if mipmap_offset == 0 {
+					break
+				}
+
+				mipmap_current := br.position
+				br.position += mipmap_offset - 16 // Hack
+
+				println(mipmap_offset)
+				subtexture_signature := br.read_u32(false)
+
+				if subtexture_signature != 0x02505854 {
+					panic('Invalid SubTexture Signature: ${subtexture_signature}')
+				}
+
+				println(br.read_u32(false)) // Width
+				println(br.read_u32(false)) // Height
+				println(br.read_u32(false)) // Format
+
+				br.position += 4
+
+				data_size := br.read_u32(false)
+				println(data_size) // DataSize
+				subtexture_data := br.read_n(data_size)
+
+				br.position = mipmap_current
+				exit(1)
+			}
+		}
+
 		br.position = current
 		exit(1)
 	}
