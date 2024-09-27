d81a88233940b3bcd65ff34306856c338184f18d
xJunko <fireredz69420@gmail.com>
2024-09-27 22:53:57 +0800

    FARC: Fix compile error with clang/gcc

--- src/divalib/archives/farc/read.v
+++ src/divalib/archives/farc/read.v
@@ -4,25 +4,29 @@ import os
 import structs
 import divalib.io
 
+const basic_archive = structs.BasicArchive{}
+const compressed_archive = structs.CompressedArchive{}
+const future_archive = structs.FutureArchive{}
+
 pub fn read(path string) !&structs.IArchive {
 	mut raw_bytes := os.read_bytes(path)!
 	mut br := io.BinaryReader.from_bytes(raw_bytes)
 
 	header := br.read_string(.length, 4)
 	match header {
-		string(structs.BasicArchive.get_header()) {
+		basic_archive.get_header() {
 			mut ba := structs.BasicArchive{}
 			ba.read(mut br)!
 
 			return ba
 		}
-		string(structs.CompressedArchive.get_header()) {
+		compressed_archive.get_header() {
 			mut ca := structs.CompressedArchive{}
 			ca.read(mut br)!
 
 			return ca
 		}
-		string(structs.FutureArchive.get_header()) {
+		future_archive.get_header() {
 			mut fa := structs.FutureArchive{}
 			fa.read(mut br)!
 
