c115d426722d730f7ff4c7fae1e088672e0de3dd
xJunko <fireredz69420@gmail.com>
2024-09-29 02:59:26 +0800

    DXT5: Fix channels being flipped

--- src/divalib/thirdparty/dxt/dxt.c.v
+++ src/divalib/thirdparty/dxt/dxt.c.v
@@ -20,11 +20,24 @@ pub fn decompress_dxt5_to_rgba(width u64, height u64, src_buf []u8, mut out_buf 
 	decompress_dxt5(width, height, src_buf, packed_rgba_pixels)
 
 	for j, color in packed_rgba_pixels {
-		out_buf[j * 4] = u8(color & 0xFF)
-		out_buf[j * 4 + 1] = u8((color >> 8) & 0xFF)
-		out_buf[j * 4 + 2] = u8((color >> 16) & 0xFF)
-		out_buf[j * 4 + 3] = u8((color >> 24) & 0xFF)
+		out_buf[j * 4] = u8((color >> 24) & 0xFF)
+		out_buf[j * 4 + 1] = u8((color >> 16) & 0xFF)
+		out_buf[j * 4 + 2] = u8((color >> 8) & 0xFF)
+		out_buf[j * 4 + 3] = u8(color & 0xFF)
 	}
+
+	// Flip horizontally
+	// for y in 0 .. height {
+	// 	for x in 0 .. width / 2 {
+	// 		for c in 0 .. 4 {
+	// 			i0 := (y * width + x) * 4 + c
+	// 			i1 := (y * width + width - x - 1) * 4 + c
+	// 			tmp := out_buf[i0]
+	// 			out_buf[i0] = out_buf[i1]
+	// 			out_buf[i1] = tmp
+	// 		}
+	// 	}
+	// }
 
 	unsafe {
 		packed_rgba_pixels.free()
