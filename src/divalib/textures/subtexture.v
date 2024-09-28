d1217b8454e550fff8028bebd7ed8e5fa12fcc52
xJunko <fireredz69420@gmail.com>
2024-09-29 02:51:04 +0800

    SubTexture: Working DXT5 texture

--- src/divalib/textures/subtexture.v
+++ src/divalib/textures/subtexture.v
@@ -1,6 +1,7 @@
 module textures
 
 import divalib.io
+import divalib.thirdparty.dxt
 
 pub struct SubTexture {
 pub mut:
@@ -27,17 +28,30 @@ pub fn (mut sub_texture SubTexture) read(mut br io.BinaryReader) {
 	sub_texture.data = br.read_n(data_size)
 }
 
-pub fn (mut sub_texture SubTexture) decode() []u8 {
-	// Decodes the .data into raw RGBA data
-
-	src_data_ptr := &sub_texture.data
+pub fn (mut sub_texture SubTexture) decode() ([]u8, int) {
+	mut channel_count := 0
 
 	if !sub_texture.format.is_compressed() {
-		// lucky me
-		panic('Unimplemented')
+		// This should be easy, so I'm leaving it for later
+		panic('Unimplemented uncompressed texture format')
 	} else {
-		// directx format, here goes nothing, total hell.
+		// This is the hard part
+		// This will be slowly implemented
+		// For now we only support DXT5
+		if sub_texture.format != .dxt5 {
+			panic('Unimplemeted Texture Format: ${sub_texture.format}')
+		}
+
+		match sub_texture.format {
+			.dxt5 {
+				mut rgba_pixels := []u8{len: int(sub_texture.width * sub_texture.height * 16)}
+				dxt.decompress_dxt5_to_rgba(sub_texture.width, sub_texture.height, sub_texture.data, mut
+					rgba_pixels)
+				return rgba_pixels, channel_count
+			}
+			else {}
+		}
 	}
 
-	return []u8{}
+	return []u8{}, channel_count
 }
