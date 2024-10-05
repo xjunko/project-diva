ca316e4021861642cef096a60bd6eec5261e0b16
xJunko <fireredz69420@gmail.com>
2024-10-05 16:03:34 +0800

    divalib: add `microui`

---
+++ src/divalib/thirdparty/microui/enums/enums.c.v
@@ -0,0 +1,27 @@
+module enums
+
+pub const clip_part = 1
+pub const clip_all = 2
+
+pub const command_jump = 1
+pub const command_clip = 2
+pub const command_rect = 3
+pub const command_text = 4
+pub const command_icon = 5
+pub const command_max = 6
+
+pub const color_text = 0
+pub const color_border = 1
+pub const color_windowbg = 2
+pub const color_titlebg = 3
+pub const color_titletext = 4
+pub const color_panelbg = 5
+pub const color_button = 6
+pub const color_buttonhover = 7
+pub const color_buttonfocus = 8
+pub const color_base = 9
+pub const color_basehover = 10
+pub const color_basefocus = 11
+pub const color_scrollbase = 12
+pub const color_scrollthumb = 13
+pub const color_max = 14
