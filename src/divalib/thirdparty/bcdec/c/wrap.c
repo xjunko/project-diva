31d1a835fe3b23d4a7b2efb9c60efc73599ecd76
xJunko <fireredz69420@gmail.com>
2024-10-07 20:31:46 +0800

    jnko_decode_ycbcr: bit more accurate colours (still wrong)

--- src/divalib/thirdparty/bcdec/c/wrap.c
+++ src/divalib/thirdparty/bcdec/c/wrap.c
@@ -5,119 +5,161 @@
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
-char* jnko_dxt1(char* src, int w, int h) {
-  char* dst = (char*)malloc(w * h * 4);
-  char* dst_ptr = dst;
+char* jnko_dxt1(char* src, int w, int h)
+{
+    char* dst = (char*)malloc(w * h * 4);
+    char* dst_ptr = dst;
 
-  for (int i = 0; i < h; i += 4) {
-    for (int j = 0; j < w; j += 4) {
-      dst_ptr = dst + (i * w + j) * 4;
-      bcdec_bc1(src, dst_ptr, w * 4);
-      src += 8;
+    for (int i = 0; i < h; i += 4) {
+        for (int j = 0; j < w; j += 4) {
+            dst_ptr = dst + (i * w + j) * 4;
+            bcdec_bc1(src, dst_ptr, w * 4);
+            src += 8;
+        }
     }
-  }
 
-  return dst;
+    return dst;
 }
 
-char* jnko_dxt3(char* src, int w, int h) {
-  char* dst = (char*)malloc(w * h * 4);
-  char* dst_ptr = dst;
+char* jnko_dxt3(char* src, int w, int h)
+{
+    char* dst = (char*)malloc(w * h * 4);
+    char* dst_ptr = dst;
 
-  for (int i = 0; i < h; i += 4) {
-    for (int j = 0; j < w; j += 4) {
-      dst_ptr = dst + (i * w + j) * 4;
-      bcdec_bc2(src, dst_ptr, w * 4);
-      src += 16;
+    for (int i = 0; i < h; i += 4) {
+        for (int j = 0; j < w; j += 4) {
+            dst_ptr = dst + (i * w + j) * 4;
+            bcdec_bc2(src, dst_ptr, w * 4);
+            src += 16;
+        }
     }
-  }
 
-  return dst;
+    return dst;
 }
 
-char* jnko_dxt5(char* src, int w, int h) {
-  char* dst = (char*)malloc(w * h * 4);
-  char* dst_ptr = dst;
+char* jnko_dxt5(char* src, int w, int h)
+{
+    char* dst = (char*)malloc(w * h * 4);
+    char* dst_ptr = dst;
 
-  for (int i = 0; i < h; i += 4) {
-    for (int j = 0; j < w; j += 4) {
-      dst_ptr = dst + (i * w + j) * 4;
-      bcdec_bc3(src, dst_ptr, w * 4);
-      src += 16;
+    for (int i = 0; i < h; i += 4) {
+        for (int j = 0; j < w; j += 4) {
+            dst_ptr = dst + (i * w + j) * 4;
+            bcdec_bc3(src, dst_ptr, w * 4);
+            src += 16;
+        }
     }
-  }
 
-  return dst;
+    return dst;
 }
 
-char* jnko_ati1(char* src, int w, int h) {
-  char* dst = (char*)malloc(w * h);
-  char* dst_ptr = dst;
+char* jnko_ati1(char* src, int w, int h)
+{
+    char* dst = (char*)malloc(w * h);
+    char* dst_ptr = dst;
 
-  for (int i = 0; i < h; i += 4) {
-    for (int j = 0; j < w; j += 4) {
-      dst_ptr = dst + (i * w + j);
-      bcdec_bc4(src, dst_ptr, w);
-      src += 8;
+    for (int i = 0; i < h; i += 4) {
+        for (int j = 0; j < w; j += 4) {
+            dst_ptr = dst + (i * w + j);
+            bcdec_bc4(src, dst_ptr, w);
+            src += 8;
+        }
     }
-  }
 
-  return dst;
+    return dst;
 }
 
-char* jnko_ati2(char* src, int w, int h) {
-  char* dst = (char*)malloc(w * h * 2);
-  char* dst_ptr = dst;
+char* jnko_ati2(char* src, int w, int h)
+{
+    char* dst = (char*)malloc(w * h * 2);
+    char* dst_ptr = dst;
 
-  for (int i = 0; i < h; i += 4) {
-    for (int j = 0; j < w; j += 4) {
-      dst_ptr = dst + (i * w + j) * 2;
-      bcdec_bc5(src, dst_ptr, w * 2);
-      src += 16;
+    for (int i = 0; i < h; i += 4) {
+        for (int j = 0; j < w; j += 4) {
+            dst_ptr = dst + (i * w + j) * 2;
+            bcdec_bc5(src, dst_ptr, w * 2);
+            src += 16;
+        }
     }
-  }
 
-  return dst;
+    return dst;
 }
 
+// based on MikuMikuLibrary/MikuMikuLibrary.Native/TextureDecoderCore.cpp
+// https://github.com/blueskythlikesclouds/MikuMikuLibrary/blob/f0e5ebc72358bd3d18ad26993eade5b2888ed7d8/MikuMikuLibrary.Native/TextureDecoderCore.cpp#L97
+// Do note that the colors are still somewhat off, but it's whatever, good enough.
 char* jnko_decode_ycbcr(char* luminance, char* chroma, int w, int h,
-                        int channels, int chroma_channels) {
-  char* final_pixels = (char*)malloc(w * h * 4 * sizeof(unsigned char));
+    int channels, int chroma_channels)
+{
+    char* final_pixels = (char*)malloc(w * h * 4 * sizeof(unsigned char));
 
-  if (final_pixels == NULL) {
-    printf("[NATIVE] Failed to allocate memory for decoding ycbcr!\n");
-    return NULL;
-  }
-
-  for (int y = 0; y < h; ++y) {
-    for (int x = 0; x < w; ++x) {
-      unsigned char Y = (unsigned char)luminance[(y * w + x) * 2];
-
-      int chroma_x = x / 2;
-      int chroma_y = y / 2;
-
-      unsigned char Cb =
-          (unsigned char)chroma[(chroma_y * (w / 2) + chroma_x) * 2];
-      unsigned char Cr =
-          (unsigned char)chroma[(chroma_y * (w / 2) + chroma_x) * 2 + 1];
-
-      float R = Y + 1.402f * (Cr - 128);
-      float G = Y - 0.344136f * (Cb - 128) - 0.714136f * (Cr - 128);
-      float B = Y + 1.772f * (Cb - 128);
-
-      final_pixels[(y * w + x) * 4 + 0] =
-          (unsigned char)fminf(fmaxf(R, 0), 255);
-      final_pixels[(y * w + x) * 4 + 1] =
-          (unsigned char)fminf(fmaxf(G, 0), 255);
-      final_pixels[(y * w + x) * 4 + 2] =
-          (unsigned char)fminf(fmaxf(B, 0), 255);
-      final_pixels[(y * w + x) * 4 + 3] =
-          (unsigned char)luminance[(y * w + x) * 2 + 1];
+    if (final_pixels == NULL) {
+        printf("[NATIVE] Failed to allocate memory for decoding ycbcr!\n");
+        return NULL;
     }
-  }
 
-  return final_pixels;
+    for (int y = 0; y < h; ++y) {
+        for (int x = 0; x < w; ++x) {
+            int chroma_x = x / 2;
+            int chroma_y = y / 2;
+
+            unsigned char cur_lum = (unsigned char)luminance[(y * w + x) * 2];
+            unsigned char cur_chroma_blue = (unsigned char)chroma[(chroma_y * (w / 2) + chroma_x) * 2];
+            unsigned char cur_chroma_red = (unsigned char)chroma[(chroma_y * (w / 2) + chroma_x) * 2 + 1];
+
+            float R = cur_lum + 1.5748f * ((cur_chroma_red * 1.003922f) - 128.501895f);
+            float G = cur_lum - 0.1873f * ((cur_chroma_blue * 1.003922f) - 128.501895) - 0.4681f * ((cur_chroma_red * 1.003922f) - 128.501895f);
+            float B = cur_lum + 1.8556f * ((cur_chroma_blue * 1.003922f) - 128.501895f);
+
+            final_pixels[(y * w + x) * 4 + 0] = (unsigned char)fminf(fmaxf(R, 0), 255);
+            final_pixels[(y * w + x) * 4 + 1] = (unsigned char)fminf(fmaxf(G, 0), 255);
+            final_pixels[(y * w + x) * 4 + 2] = (unsigned char)fminf(fmaxf(B, 0), 255);
+            final_pixels[(y * w + x) * 4 + 3] = (unsigned char)luminance[(y * w + x) * 2 + 1];
+        }
+    }
+
+    return final_pixels;
 }
+
+// same as below, but without the resizing. this is the original version
+// char* jnko_decode_ycbcr(char* luminance, char* chroma, int w, int h,
+//                         int channels, int chroma_channels) {
+//   char* final_pixels = (char*)malloc(w * h * 4 * sizeof(unsigned char));
+
+//   if (final_pixels == NULL) {
+//     printf("[NATIVE] Failed to allocate memory for decoding ycbcr!\n");
+//     return NULL;
+//   }
+
+//   for (int y = 0; y < h; ++y) {
+//     for (int x = 0; x < w; ++x) {
+//       unsigned char Y = (unsigned char)luminance[(y * w + x) * 2];
+
+//       int chroma_x = x / 2;
+//       int chroma_y = y / 2;
+
+//       unsigned char Cb =
+//           (unsigned char)chroma[(chroma_y * (w / 2) + chroma_x) * 2];
+//       unsigned char Cr =
+//           (unsigned char)chroma[(chroma_y * (w / 2) + chroma_x) * 2 + 1];
+
+//       float R = Y + 1.402f * (Cr - 128);
+//       float G = Y - 0.344136f * (Cb - 128) - 0.714136f * (Cr - 128);
+//       float B = Y + 1.772f * (Cb - 128);
+
+//       final_pixels[(y * w + x) * 4 + 0] =
+//           (unsigned char)fminf(fmaxf(R, 0), 255);
+//       final_pixels[(y * w + x) * 4 + 1] =
+//           (unsigned char)fminf(fmaxf(G, 0), 255);
+//       final_pixels[(y * w + x) * 4 + 2] =
+//           (unsigned char)fminf(fmaxf(B, 0), 255);
+//       final_pixels[(y * w + x) * 4 + 3] =
+//           (unsigned char)luminance[(y * w + x) * 2 + 1];
+//     }
+//   }
+
+//   return final_pixels;
+// }
 
 // Alternative shorter version but resizes the chroma
 // char* jnko_decode_ycbcr(char* luminance, char* chroma, int w, int h,
