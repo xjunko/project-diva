771062c51865bfdccae03c7d97e9b63064fcc846
xJunko <fireredz69420@gmail.com>
2024-10-20 21:26:51 +0800

    BinaryReader: `from_bytes_mut`

--- src/divalib/io/binary_reader.v
+++ src/divalib/io/binary_reader.v
@@ -192,6 +192,15 @@ pub fn BinaryReader.from_bytes(data []u8) &BinaryReader {
 	return br
 }
 
+pub fn BinaryReader.from_bytes_mut(mut data []u8) &BinaryReader {
+	mut br := &BinaryReader{}
+
+	br.data = unsafe { mut data }
+	br.position = 0
+
+	return br
+}
+
 pub fn BinaryReader.from_bytes_clone(data []u8) &BinaryReader {
 	mut br := &BinaryReader{}
 
