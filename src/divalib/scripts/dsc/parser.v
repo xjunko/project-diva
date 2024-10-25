0c84131f774011aa13008a11ecfb31981949ea5d
xJunko <fireredz69420@gmail.com>
2024-10-25 16:51:06 +0800

    DSCParser: Implement `normalize` for easier timing

--- src/divalib/scripts/dsc/parser.v
+++ src/divalib/scripts/dsc/parser.v
@@ -4,15 +4,75 @@ import os
 import encoding.binary
 import dsc.opcodes
 
+pub struct DSCEvent {
+pub mut:
+	action     string
+	start_time f64
+	end_time   f64
+	values     []int
+}
+
 pub struct DSCParser {
 pub mut:
-	commands []opcodes.OPCode
+	commands []DSCEvent
+}
+
+fn (mut parser DSCParser) normalize(commands []opcodes.OPCode) ! {
+	// Emulates the game loop to get the right timing and everything.
+	mut current_line := 0
+	mut current_time := f64(0.0)
+	mut current_offset := f64(1000.0)
+
+	for current_line < commands.len {
+		current_opcode := commands[current_line]
+
+		match current_opcode.action {
+			'TIME' {
+				current_time = DSCParser.diva_time_to_standard_milliseconds(current_opcode.arguments[0])
+			}
+			'LYRIC' {
+				parser.commands << DSCEvent{
+					action:     current_opcode.action
+					start_time: current_time
+					end_time:   current_time
+					values:     current_opcode.arguments
+				}
+			}
+			'MUSIC_PLAY' {
+				parser.commands << DSCEvent{
+					action:     current_opcode.action
+					start_time: current_time
+					end_time:   current_time
+				}
+			}
+			'TARGET' {
+				parser.commands << DSCEvent{
+					action:     current_opcode.action
+					start_time: current_time + current_offset
+					end_time:   current_time + current_offset
+					values:     current_opcode.arguments
+				}
+			}
+			'BAR_TIME_SET' {
+				current_offset = f64(current_opcode.arguments[0]) * 4.0
+			}
+			'TARGET_FLYING_TIME' {
+				current_offset = f64(current_opcode.arguments[0])
+			}
+			else {}
+		}
+
+		current_line++
+	}
+
+	parser.commands.sort(a.start_time < b.start_time)
 }
 
 pub fn DSCParser.from_file(path string) !DSCParser {
 	raw_bytes := os.read_bytes(path)!
 
 	mut diva_script := DSCParser{}
+	mut commands := []opcodes.OPCode{}
 
 	// Start reading, 4 bytes for each header
 	for i := 0; i < raw_bytes.len; i += 4 {
@@ -28,9 +88,11 @@ pub fn DSCParser.from_file(path string) !DSCParser {
 			}
 
 			// NOTE: Empty arguments OPCodes are also included, we might not need to include them
-			diva_script.commands << current_operation
+			commands << current_operation
 		}
 	}
+
+	diva_script.normalize(commands)!
 
 	return diva_script
 }
