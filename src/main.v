c5b70450e180f86304eac54f7cab9f63d2eb4ad8
xJunko <fireredz69420@gmail.com>
2024-10-25 16:02:33 +0800

    Project-Diva: Fix Conflicting dependecies

--- src/main.v
+++ src/main.v
@@ -1,12 +1,15 @@
 module main
 
-import divagame
+// import divagame
 import divacmd
+import divaplayer
 
 fn main() {
 	$if divacmd ? {
 		divacmd.run()!
+	} $else $if divaplayer ? {
+		divaplayer.run()
 	} $else {
-		divagame.run()
+		// divagame.run()
 	}
 }
